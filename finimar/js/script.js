e7e4a38237cd36816bef42e9ca3fd04deaaf0426
Yassine-Zaazaa <yassine.zaazaa@um6p.ma>
2022-03-05 11:08:06 +0100

    Commit

--- finimar/js/script.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navLinks = document.getElementsByClassName('nav__links')[0]
-const header = document.getElementsByClassName('header_wrapper')[0]
-
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active')
-  header.classList.toggle('active')
-})
